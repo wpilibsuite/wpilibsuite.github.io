d32e2b21b548cbb389a74f93f894c769b0aae70e
PeterJohnson <johnson.peter@gmail.com>
2021-07-13 18:42:10 +0000

    Deploying to main from  @ e4dc3908bbef1b43d092b260cf11d428f91cbb3a 🚀

--- allwpilib/docs/development/cpp/structbasic__data.js
+++ allwpilib/docs/development/cpp/structbasic__data.js
@@ -1,10 +1,10 @@
 var structbasic__data =
 [
-    [ "digits", "structbasic__data.html#a77b5a9400242639f76162742087d955d", null ],
+    [ "digits", "structbasic__data.html#a5012a7b8b0b56e512c66039b65daeb3d", null ],
     [ "hex_digits", "structbasic__data.html#a1915cc272d079f0335fcf7e558c8a495", null ],
-    [ "left_padding_shifts", "structbasic__data.html#aacea3b25006121bce42ebf36a4578ccb", null ],
+    [ "left_padding_shifts", "structbasic__data.html#a0f56fc3864b43703b33aa9e62a1de473", null ],
     [ "log10_2_significand", "structbasic__data.html#a9e30f7176a169465975ba94e47fc6337", null ],
     [ "prefixes", "structbasic__data.html#a7194648be6d81983cc0805e764e4ae56", null ],
-    [ "right_padding_shifts", "structbasic__data.html#a05197889b8b713b1f7aa40a08a73f737", null ],
-    [ "signs", "structbasic__data.html#a526c21f936e3d04f2a90e7acd600561a", null ]
+    [ "right_padding_shifts", "structbasic__data.html#ab309635873089a43982492728148b4dd", null ],
+    [ "signs", "structbasic__data.html#a5e58f495e74af2bd0125f1344851917b", null ]
 ];
