2c800ca97c583883166c443c1787751eb755c1f1
PeterJohnson <johnson.peter@gmail.com>
2022-01-05 06:34:21 +0000

    Deploying to main from  @ 22c4da152e90ca0a6d522b7bafe802a97ceef226 🚀

--- allwpilib/docs/development/cpp/classfrc_1_1sim_1_1_a_d_i_s16448___i_m_u_sim.js
+++ allwpilib/docs/development/cpp/classfrc_1_1sim_1_1_a_d_i_s16448___i_m_u_sim.js
@@ -6,5 +6,8 @@ var classfrc_1_1sim_1_1_a_d_i_s16448___i_m_u_sim =
     [ "SetAccelZ", "classfrc_1_1sim_1_1_a_d_i_s16448___i_m_u_sim.html#a85731cdba488c6397996f768f2e10297", null ],
     [ "SetGyroAngleX", "classfrc_1_1sim_1_1_a_d_i_s16448___i_m_u_sim.html#ae34f7c5dc88889a986db6cb9d31c04d3", null ],
     [ "SetGyroAngleY", "classfrc_1_1sim_1_1_a_d_i_s16448___i_m_u_sim.html#a72660b259be8d1dfc66de24100c35c1d", null ],
-    [ "SetGyroAngleZ", "classfrc_1_1sim_1_1_a_d_i_s16448___i_m_u_sim.html#a0652207da316c235c7d2db1f805f73c0", null ]
+    [ "SetGyroAngleZ", "classfrc_1_1sim_1_1_a_d_i_s16448___i_m_u_sim.html#a0652207da316c235c7d2db1f805f73c0", null ],
+    [ "SetGyroRateX", "classfrc_1_1sim_1_1_a_d_i_s16448___i_m_u_sim.html#a5d3b01fb8724cf1af757e5375221d65d", null ],
+    [ "SetGyroRateY", "classfrc_1_1sim_1_1_a_d_i_s16448___i_m_u_sim.html#a659441fc2276c95f13d064c3a13fed56", null ],
+    [ "SetGyroRateZ", "classfrc_1_1sim_1_1_a_d_i_s16448___i_m_u_sim.html#ad5ec3279dcfc78456b47211fd716fefb", null ]
 ];
--- allwpilib/docs/development/cpp/classfrc_1_1sim_1_1_a_d_i_s16470___i_m_u_sim.js
+++ allwpilib/docs/development/cpp/classfrc_1_1sim_1_1_a_d_i_s16470___i_m_u_sim.js
@@ -6,5 +6,8 @@ var classfrc_1_1sim_1_1_a_d_i_s16470___i_m_u_sim =
     [ "SetAccelZ", "classfrc_1_1sim_1_1_a_d_i_s16470___i_m_u_sim.html#aea7401978e4a300f67adb5779d2323f9", null ],
     [ "SetGyroAngleX", "classfrc_1_1sim_1_1_a_d_i_s16470___i_m_u_sim.html#a4cad818917fcfbf35c213361eab9ea4a", null ],
     [ "SetGyroAngleY", "classfrc_1_1sim_1_1_a_d_i_s16470___i_m_u_sim.html#a0b75ac5dfd98760017f1344142b55bd2", null ],
-    [ "SetGyroAngleZ", "classfrc_1_1sim_1_1_a_d_i_s16470___i_m_u_sim.html#a4a23fab8706e0b79da8237cd66f5a817", null ]
+    [ "SetGyroAngleZ", "classfrc_1_1sim_1_1_a_d_i_s16470___i_m_u_sim.html#a4a23fab8706e0b79da8237cd66f5a817", null ],
+    [ "SetGyroRateX", "classfrc_1_1sim_1_1_a_d_i_s16470___i_m_u_sim.html#a0d68a73a8d2461e3f6534962e201df36", null ],
+    [ "SetGyroRateY", "classfrc_1_1sim_1_1_a_d_i_s16470___i_m_u_sim.html#ad73a8677314d50c42c457fee555fd46a", null ],
+    [ "SetGyroRateZ", "classfrc_1_1sim_1_1_a_d_i_s16470___i_m_u_sim.html#a3c2bc871fe06ec106002a9c1688b00a2", null ]
 ];
