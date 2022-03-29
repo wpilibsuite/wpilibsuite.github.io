3bd665d62476295890749d72007db45fa949d134
PeterJohnson <johnson.peter@gmail.com>
2022-03-29 18:40:34 +0000

    Deploying to main from  @ c8905ec29aa3299c941138b015281bef0bf9d815 🚀

--- allwpilib/docs/development/cpp/classfrc_1_1_implicit_model_follower.js
+++ allwpilib/docs/development/cpp/classfrc_1_1_implicit_model_follower.js
@@ -1,7 +1,7 @@
 var classfrc_1_1_implicit_model_follower =
 [
-    [ "ImplicitModelFollower", "classfrc_1_1_implicit_model_follower.html#a0820c730ca525831d327f1cc143f13d4", null ],
-    [ "ImplicitModelFollower", "classfrc_1_1_implicit_model_follower.html#a47ee2d6a710f8ca2e6b58cddd86a6cdb", null ],
+    [ "ImplicitModelFollower", "classfrc_1_1_implicit_model_follower.html#aeba1f741d0732f3813ee9aa794f102b4", null ],
+    [ "ImplicitModelFollower", "classfrc_1_1_implicit_model_follower.html#ae484c7060ae07eece8c84daa34f9ac24", null ],
     [ "Calculate", "classfrc_1_1_implicit_model_follower.html#a104c80df04aaefbbea2b84e21f0fa89c", null ],
     [ "Reset", "classfrc_1_1_implicit_model_follower.html#aa87deafd5153efd565649f5d2c3461e0", null ],
     [ "U", "classfrc_1_1_implicit_model_follower.html#a35f7f76d4c7aa77596f08d6293dcb469", null ],
