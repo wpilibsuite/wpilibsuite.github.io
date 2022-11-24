f51354953ddd833b662cc7ac2c8e7c77f806b5fa
PeterJohnson <johnson.peter@gmail.com>
2022-11-24 07:26:15 +0000

    Deploying to main from  @ d5200db6cd5cc32d44f4433be2cac2b6d8b9d47a 🚀

--- allwpilib/docs/development/cpp/classfrc_1_1_holonomic_drive_controller.js
+++ allwpilib/docs/development/cpp/classfrc_1_1_holonomic_drive_controller.js
@@ -2,8 +2,8 @@ var classfrc_1_1_holonomic_drive_controller =
 [
     [ "HolonomicDriveController", "classfrc_1_1_holonomic_drive_controller.html#a42d70c4f7b4987b6878fd09581bce1e1", null ],
     [ "AtReference", "classfrc_1_1_holonomic_drive_controller.html#a541440376f1de7e2ca3b98a38f90d699", null ],
-    [ "Calculate", "classfrc_1_1_holonomic_drive_controller.html#a50694abd7f07c2a99f5166347360bb01", null ],
-    [ "Calculate", "classfrc_1_1_holonomic_drive_controller.html#ac79f6e63d499f28f8952a2c075c9d18b", null ],
+    [ "Calculate", "classfrc_1_1_holonomic_drive_controller.html#a302f0e3193618b594f47f59cc7ebd8d7", null ],
+    [ "Calculate", "classfrc_1_1_holonomic_drive_controller.html#a2b7e285b37f867b886593de6975e5d74", null ],
     [ "SetEnabled", "classfrc_1_1_holonomic_drive_controller.html#a472ef7c9cb973c89736eda8bfb4589d9", null ],
     [ "SetTolerance", "classfrc_1_1_holonomic_drive_controller.html#a3c111c2c83d6277b1126cab5ca95d518", null ]
 ];
